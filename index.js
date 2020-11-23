327550054cefcb4b4280471937f75af2baeeed2c
Thomas Longsdorf <tlongsdorf@corp.ecaos.com>
2020-11-23 13:28:00 -0500

    Adding on a Server Element.  Stubbed out for handling GET requests. Added 4 Test Cases as a Playground. test=0 - Red test=1 - Green test=2 - Blue test=3 - White test=4 - Flip between Blue/White default - Multi-color chase

--- index.js
+++ index.js
@@ -1,49 +1,101 @@
-const Protocol = require('./src/protocol')
-const net = require('net')
-const argv = require('yargs').argv
-const debug = require('debug')('illuminate:main')
-const parse = require('color-parse')
+const Protocol = require('./src/protocol');
+const net = require('net');
+const debug = require('debug')('illuminate:main');
+const parse = require('color-parse');
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
-const hosts = argv._[0].split(',')
-const command = argv._[1]
-const args = argv._.slice(2)
+const port = 3000;
+const IP = '192.168.1.XXX,192.168.1.XXX'; // Comma delimited string of Lighting Module IP address - 192.168.1.XXX';
 
-for (const host of hosts) {
-  const client = new net.Socket()
-  console.log(host, command, ...args)
-  client.connect(5577, host, function () {
-    const protocol = new Protocol(client)
-    let params = []
-    let response = null
+app.get('/', function (request, response) {
+  console.log('GET /');
+  console.log(request.query);
 
-    // console.log(colors)
-    if (command in protocol) {
-      switch (command) {
-        case 'updateColor':
-          if (args.length === 1) {
-            params = [parse(args[0]).values.concat(0)] // [ R, G, B, 0 ]
-          } else {
-            params = [args.map((number) => parseInt(number))]
-          }
-          break
-        case 'updateColors':
-          params = [args.map((color) => parse(color).values)]
-          break
-        case 'custom':
-          params.push(args[0])
-          params.push(args[1])
-          params = params.concat(args.slice(2).map((color) => parse(color).values))
-          break
-        case 'program':
-          params = args.map((number) => parseInt(number, 10))
-          break
-      }
-      debug('parameters', params)
-      response = protocol[command].apply(protocol, params)
+  if (request.query.hasOwnProperty('test')) {
+    const test = request.query.test;
+    switch (test) {
+      case '0':
+        execute(IP, 'updateColor', { r: 255, g: 0, b: 0, w: 0 });
+        break;
+      case '1':
+        execute(IP, 'updateColor', { r: 0, g: 255, b: 0, w: 0 });
+        break;
+      case '2':
+        execute(IP, 'updateColor', { r: 0, g: 0, b: 255, w: 0 });
+        break;
+      case '3':
+        execute(IP, 'updateColor', { r: 0, g: 0, b: 0, w: 255 });
+        break;
+      case '4':
+        // Flips between Blue and White
+        execute(IP, 'updateColor', { r: 0, g: 0, b: 255, w: 0 });
+        setTimeout(() => {
+          execute(IP, 'updateColor', { r: 0, g: 0, b: 0, w: 255 });
+        }, 1000, 'funky');
+
+        break;
+      default:
+        execute(IP, 'program', { program: 1, speed: 50 });
+        break;
     }
+  }
 
-    Promise.resolve(response).then(() => {
-      client.destroy()
-    })
-  })
+  response.send(request.query);
+});
+
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
+
+/**
+ * Sends Config Message to Light Control Module via IP
+ * @param ips - Comma Delimited List of Module IPs
+ * @param type - Command Type ( updateColor, program, custom)
+ * @param params - Params corresponding to defined Type.
+ */
+function execute (ips, type, params) {
+  const hosts = ips.split(',');
+  const command = type;
+  const args = params;
+
+  for (const host of hosts) {
+    const client = new net.Socket();
+    console.log(host, command, args);
+    client.connect(5577, host, function () {
+      const protocol = new Protocol(client);
+      let paramsArr = [];
+      let response = null;
+
+      // console.log(colors)
+      if (command in protocol) {
+        switch (command) {
+          case 'updateColor':
+            if (args.length === 1) {
+              paramsArr = [parse(args[0]).values.concat(0)]; // [ R, G, B, 0 ]
+            } else {
+              paramsArr = [Object.values(args)];
+            }
+            break;
+          case 'updateColors':
+            paramsArr = [Object.values(args)];
+            break;
+          case 'custom':
+            paramsArr.push(args[0]);
+            paramsArr.push(args[1]);
+            paramsArr = paramsArr.concat(args.slice(2).map((color) => parse(color).values));
+            break;
+          case 'program':
+            paramsArr = Object.values(args);
+            break;
+        }
+        debug('parameters', paramsArr);
+        response = protocol[command].apply(protocol, paramsArr);
+      }
+
+      Promise.resolve(response).then(() => {
+        client.destroy();
+      });
+    });
+  }
 }
--- src/protocol.js
+++ src/protocol.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('illuminate:protocol')
+const debug = require('debug')('illuminate:protocol');
 
 module.exports = class Illuminate {
   /*
@@ -7,7 +7,7 @@ module.exports = class Illuminate {
   */
 
   constructor (client) {
-    this.client = client
+    this.client = client;
   }
 
   /* Off
@@ -26,13 +26,13 @@ module.exports = class Illuminate {
   off () {
     const response = this._receiveMessage((data) => {
       if (data[1] === 0x24) {
-        console.log('lights are off')
+        console.log('lights are off');
       }
-    })
+    });
 
-    this._sendMessage([0xcc, 0x24, 0x33])
+    this._sendMessage([0xcc, 0x24, 0x33]);
 
-    return response
+    return response;
   }
 
   /* On
@@ -51,13 +51,13 @@ module.exports = class Illuminate {
   on () {
     const response = this._receiveMessage((data) => {
       if (data[1] === 0x23) {
-        console.log('lights are on')
+        console.log('lights are on');
       }
-    })
+    });
 
-    this._sendMessage([0xcc, 0x23, 0x33])
+    this._sendMessage([0xcc, 0x23, 0x33]);
 
-    return response
+    return response;
   }
 
   /* Connect
@@ -86,17 +86,17 @@ module.exports = class Illuminate {
   status () {
     const response = this._receiveMessage((data) => {
       if (data.length > 9) {
-        if (data[2] === 0x23) { console.log('lights are on') }
-        if (data[2] === 0x24) { console.log('lights are off') }
-        console.log('program number', data[3])
-        console.log('speed', data[4])
-        console.log('red', data[6], 'green', data[7], 'blue', data[8], 'white', data[9])
+        if (data[2] === 0x23) { console.log('lights are on'); }
+        if (data[2] === 0x24) { console.log('lights are off'); }
+        console.log('program number', data[3]);
+        console.log('speed', data[4]);
+        console.log('red', data[6], 'green', data[7], 'blue', data[8], 'white', data[9]);
       }
-    })
+    });
 
-    this._sendMessage([0xef, 0x01, 0x77])
+    this._sendMessage([0xef, 0x01, 0x77]);
 
-    return response
+    return response;
   }
 
   /* Update Colors
@@ -114,44 +114,61 @@ module.exports = class Illuminate {
   */
 
   updateColor (color) {
-    this._sendMessage([0x56].concat(color).concat(0xAA))
+    this._sendMessage([0x56].concat(color).concat(0xAA));
   }
 
   warm () {
-    this.updateColor([0, 0, 0, 255])
+    this.updateColor([0, 0, 0, 255]);
   }
 
   cool () {
-    this.updateColor([255, 255, 255, 0])
+    this.updateColor([255, 255, 255, 0]);
   }
 
   bright () {
-    this.updateColor([255, 255, 255, 255])
+    this.updateColor([255, 255, 255, 255]);
   }
+
+  /* red () {
+    this.updateColor([255, 0, 0, 0])
+  }
+
+  green () {
+    this.updateColor([0, 255, 0, 0])
+  }
+
+  blue () {
+    this.updateColor([0, 0, 255, 0])
+  } */
+
+  /**
+   * Updates the Colors for the entire string
+   * @param colors
+   */
+  updateColors (colors) {
+    let ary = [0x88];
+    ary = ary.concat(colors.length);
+    for (var idx = 0; idx < colors.length; idx++) {
+      ary = ary.concat(colors[idx]);
+    }
+    ary = ary.concat(0x55);
+    this._sendMessage(ary);
+  }
+
 
   /* Update Pattern
-  *
-  * Sent
-  * Byte: 0x88
-  * Byte: 0x05 Number of colors?
-  * 3 Bytes: ff 00 00 Color 1?
-  * 3 Bytes: ff 4e 00 Color 2?
-  * 3 Bytes: 00 c8 00 Color 3
-  * 3 Bytes: 00 00 ff Color 4
-  * 3 Bytes: 80 00 80 Color 5
-  * Byte: 0x55
-  *
-  */
-
-  updateColors (colors) {
-    let ary = [0x88]
-    ary = ary.concat(colors.length)
-    for (var idx = 0; idx < colors.length; idx++) {
-      ary = ary.concat(colors[idx])
-    }
-    ary = ary.concat(0x55)
-    this._sendMessage(ary)
-  }
+*
+* Sent
+* Byte: 0x88
+* Byte: 0x05 Number of colors?
+* 3 Bytes: ff 00 00 Color 1?
+* 3 Bytes: ff 4e 00 Color 2?
+* 3 Bytes: 00 c8 00 Color 3
+* 3 Bytes: 00 00 ff Color 4
+* 3 Bytes: 80 00 80 Color 5
+* Byte: 0x55
+*
+*/
 
   /* Custom
   *
@@ -180,22 +197,22 @@ module.exports = class Illuminate {
   */
 
   custom (mode, speed, ...colors) {
-    let ary = [0x99]
+    let ary = [0x99];
     for (var idx = 0; idx < 16; idx++) {
       if (idx < colors.length) {
-        ary = ary.concat(colors[idx])
+        ary = ary.concat(colors[idx]);
       } else {
-        ary = ary.concat([1, 2, 3])
+        ary = ary.concat([1, 2, 3]);
       }
     }
-    ary = ary.concat(speed)
-    ary = ary.concat(mode + 0x39)
-    ary = ary.concat([0xff, 0x66])
-    console.log(ary)
-    this._sendMessage(ary)
+    ary = ary.concat(speed);
+    ary = ary.concat(mode + 0x39);
+    ary = ary.concat([0xff, 0x66]);
+    console.log(ary);
+    this._sendMessage(ary);
   }
 
-  /* Function
+  /* Function - Pre-Built Internal Programs 1 - 21
   *
   * Sent
   * Byte: 0xbb
@@ -205,26 +222,27 @@ module.exports = class Illuminate {
   */
 
   program (programNumber, speed) {
-    this._sendMessage([0xbb, programNumber + 0x24, speed, 0x44])
+    this._sendMessage([0xbb, programNumber + 0x24, speed, 0x44]);
   }
 
+
   _sendMessage (data) {
-    debug('sending <', ...data.map((val) => val.toString(16)), '>')
-    this.client.write(new Uint8Array(data), 'binary')
+    debug('sending <', ...data.map((val) => val.toString(16)), '>');
+    this.client.write(new Uint8Array(data), 'binary');
   }
 
   async _receiveMessage (cb) {
     const response = new Promise((resolve) => {
       this.client.on('data', (data) => {
-        debug('receieved <', ...Array.from(data.values()).map((val) => val.toString(16)), '>')
-        cb(data)
-        resolve()
-      })
+        debug('receieved <', ...Array.from(data.values()).map((val) => val.toString(16)), '>');
+        cb(data);
+        resolve();
+      });
       this.client.on('error', (error) => {
-        console.error(error.message)
-        resolve()
-      })
-    })
-    return response
+        console.error(error.message);
+        resolve();
+      });
+    });
+    return response;
   }
-}
+};
