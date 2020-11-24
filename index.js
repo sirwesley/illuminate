7a1db410189d5fff31a120da8848dba4228e4b3a
sirwesley <sirwesley@gmail.com>
2020-11-23 21:01:02 -0500

    Getting the sACN e1.31 working with the Phillips Illuminate Lighting. Works with VIXEN.

--- index.js
+++ index.js
@@ -3,13 +3,53 @@ const net = require('net');
 const debug = require('debug')('illuminate:main');
 const parse = require('color-parse');
 var express = require('express');
+var e131 = require('e131');
 var fs = require('fs');
 var app = express();
 
+const e131Port = 5568;
+let lastSlotData = 0;
 const port = 3000;
-const IP = '192.168.1.xxx,192.168.1.xxx'; // <--- insert your IPs here - 192.168.1.XXX';
+const IP = '192.168.1.XXX,192.168.1.XXX'; // <--- insert your IPs here - 192.168.1.XXX';
+
+// sACN Server listening for VIXEN data.
+var server = new e131.Server([0x0001, 0x0002], e131Port);
+server.on('listening', function () {
+  console.log('server listening on port %d, universes %j', this.port, this.universes);
+});
+server.on('packet', function (packet) {
+  var sourceName = packet.getSourceName();
+  var sequenceNumber = packet.getSequenceNumber();
+  var universe = packet.getUniverse();
+  var slotsData = packet.getSlotsData();
+
+  console.log('source="%s", seq=%d, universe=%d, slots=%d',
+    sourceName, sequenceNumber, universe, slotsData.length);
+  console.log('slots data = %s', slotsData.toString('hex'));
+
+  let configSlot = slotsData[0]; // Determines number of Color Segments
+  console.log(configSlot);
+
+  let newSlotData = slotsData.toString('hex');
+
+  if (newSlotData !== lastSlotData) {
+    if (configSlot > 1) {
+      let patternsArray = [];
+      for (var i = 0; i < configSlot; i++) {
+        const offset = i * 3;
+        patternsArray.push({ r: slotsData[(1 + offset)], g: slotsData[2 + offset], b: slotsData[3 + offset] });
+      }
+      execute(IP, 'updatePattern', patternsArray);
+    } else {
+      execute(IP, 'updateColor', { r: slotsData[1], g: slotsData[2], b: slotsData[3], w: 0 });
+    }
+    lastSlotData = newSlotData; // Update LastSlotData
+  }
+});
+
 let text = '';
 
+// RESTful endpoint for GET updates.  Primary used for Testing
 app.get('/', function (request, response) {
   if (request.query.hasOwnProperty('test')) {
     const test = request.query.test;
